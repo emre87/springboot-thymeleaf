03fdf98b782919b14fa42402ed2930ff99e7e945
Şehmus Emre DEMİRALAY <sehmusemre.demiralay@tuikmerkez.tuik.gov.tr>
2023-01-23 11:47:45 +0300

    first commit

---
+++ src/main/java/net/emre/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package net.emre.springboot.repository;
+
+import net.emre.springboot.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
